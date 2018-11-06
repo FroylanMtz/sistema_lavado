0f290395285c0c4d10fa7c3c35bb153dd24469f6
MagalyAlejandra <42451269+AlejandraDom@users.noreply.github.com>
2018-11-06 10:29:05 -0600

    Update sistema_lavado.sql

--- sistema_lavado.sql
+++ sistema_lavado.sql
@@ -65,6 +65,7 @@ CREATE TABLE `clientes` (
 
 CREATE TABLE `cupones` (
   `cupon_id` varchar(7) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
   `cliente_id` int(11) NOT NULL,
   `premio_id` int(11) NOT NULL,
   `expiracion` date NOT NULL
@@ -115,6 +116,7 @@ CREATE TABLE `promociones` (
 CREATE TABLE `visitas` (
   `visita_id` int(11) NOT NULL,
   `fecha` date NOT NULL,
+  `cupon_id` varchar(7) COLLATE utf8_bin NOT NULL,
   `cliente_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -163,7 +165,9 @@ ALTER TABLE `promociones`
 -- Indexes for table `visitas`
 --
 ALTER TABLE `visitas`
-  ADD PRIMARY KEY (`visita_id`);
+  ADD PRIMARY KEY (`visita_id`),
+  ADD KEY `visitas_ibfk_1` (`cupon_id`);
+
 --
 -- AUTO_INCREMENT for dumped tables
 --
@@ -198,9 +202,15 @@ ALTER TABLE `promociones`
 ALTER TABLE `visitas`
   MODIFY `visita_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--- AUTO_INCREMENT for table `clientes`
+--
+ALTER TABLE `clientes`
+MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- Constraints for dumped tables
 --
+
 
 --
 -- Constraints for table `cupones`
@@ -214,6 +224,7 @@ ALTER TABLE `cupones`
 -- Constraints for table `visitas`
 --
 ALTER TABLE `visitas`
+  ADD CONSTRAINT `visitas_ibfk_1` FOREIGN KEY (`cupon_id`) REFERENCES `cupones` (`cupon_id`) ON DELETE CASCADE;
   ADD CONSTRAINT `visitas_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`cliente_id`) ON DELETE CASCADE;
 COMMIT;
 
